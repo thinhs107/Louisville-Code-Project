3817166f41d0ccda1794f77c3af4ca183fe16150
Thinh Hoang <Thinhs107@yahoo.com>
2021-05-30 03:10:45 -0400

    add logic to be able to sort JSON asc

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -1,17 +1,17 @@
 //Splash Screen
 
-const splash = document.querySelector('.splash');
+//const splash = document.querySelector('.splash');
 
-document.addEventListener('DOMContentLoaded', (e)=>{
-  setTimeout(() => {
-    splash.classList.add('display-none');
-  }, 4000)
-})
+//document.addEventListener('DOMContentLoaded', (e)=>{
+//  setTimeout(() => {
+//    splash.classList.add('display-none');
+//  }, 4000)
+//})
 
 
 
 //Players Stats Tab
-function PlayersStats(evt, cityName) {
+function PlayersStats(evt, sports) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -21,31 +21,106 @@ function PlayersStats(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(sports).style.display = "block";
     evt.currentTarget.className += " active";
   }
   
-  window.onload = async function loadStats() {
+async function loadStats(q) {
     await fetch("../Stats.json")
       .then((res) => res.json())
       //.then(NBAStats => console.log(NBAStats))
       .then((NBAPlayersStats) => {
-        let NBAPlayerStatsOutput = "<h2>Top 5 Leaders in Point Per Game</h2>"
-        //loop through the JSON files (Get top 10 only)
-        for(let i = 0; i < 5; i++){
-          console.log(NBAPlayersStats.data[i]["PLAYER"]);
-          let _playerName = NBAPlayersStats.data[i]["PLAYER"]
-          let _PPG = NBAPlayersStats.data[i]["PTS"]
-          let _Image = NBAPlayersStats.data[i]["IMAGE"]
-          NBAPlayerStatsOutput += `<ul>
-                                      <li><img src = "${_Image}"></li>      
-                                      <li> Player Name: ${_playerName}</li>
-                                      <li> PPT: ${_PPG}</li>
+        let _NBAPlayerStatsOutput = "";
+        let _DESC = "";
+        let _STAT = "";
+        let _SortValue = [];
+        let _TopPlayers = 5;
+        if(q === 'PTS'){
+          //Create another a function that sort and return top players
+          _DESC = 'Point Per Game'
+          _STAT = 'PTS'
+          _NBAPlayerStatsOutput = `<h3> Top 5 Leaders in ${_DESC}:</h3>`
+          _SortValue = sortPlayer(NBAPlayersStats, _STAT)
+        }
+        if(q === 'REB'){
+          _DESC = 'Rebound'
+          _STAT = 'REB'
+          _NBAPlayerStatsOutput = `<h3> Top 5 Leaders in ${_DESC}:</h3>`
+          _SortValue = sortPlayer(NBAPlayersStats, _STAT)
+          //console.log(_SortValue);
+        }
+        if(q === 'AST'){
+          _DESC = 'Assits'
+          _STAT = 'AST'
+          _SortValue = sortPlayer(NBAPlayersStats, _STAT)
+          _NBAPlayerStatsOutput = `<h3> Top 5 Leaders in ${_DESC}:</h3>`
+        }
+        //loop through the JSON files (Get top 5 players only)
+        for(let i = 0; i < _TopPlayers; i++){
+          //console.log(NBAPlayersStats.data[i]["PLAYER"]);
+          //<li><img src = "${_Image}"></li>    
+          let _playerName = _SortValue[i]["PLAYER"]
+          let _playerStats = _SortValue[i][_STAT]
+          let _Image = _SortValue[i]["IMAGE"]
+          _NBAPlayerStatsOutput += `<ul>      
+                                      <li> Player Name: ${_playerName}</li>  
+                                      <li> ${_DESC}: ${_playerStats}</li>                                   
                                    </ul>`
   
           ;
         }
-        document.getElementById('NBAPlayerStatsOutput').innerHTML = NBAPlayerStatsOutput;
+        document.getElementById('NBAPlayerStatsPPT').innerHTML = _NBAPlayerStatsOutput;
       })
   };
+
+  
+  function topPlayers(id){
+    console.log(id)
+    let TopPlayers;
+    if(id === 'top05'){
+      return TopPlayers = 5;
+    }
+    if(id === 'top10'){
+      return TopPlayers = 10;
+    }
+    if(id === 'top15'){
+      return TopPlayers = 15;
+    }
+    if(id === 'top20'){
+      return TopPlayers = 20;
+    }
+
+  }
+  //Drop Down
+  function PlayersStatDropDown(){
+    document.getElementById("NBADropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  //Sort Player
+  function sortPlayer(NBAPlayersStats,STAT){
+    var SortValue = [];
+    for(let j = 0; j < NBAPlayersStats.data.length; j++){
+      SortValue.push(NBAPlayersStats.data[j])
+    }
+    //console.log(SortValue);
+    SortValue.sort(function (a, b) {
+      return  b[STAT] - a[STAT];
+      //console.log(a - b);
+    });
+    //console.log(SortValue);
+    return SortValue;
+  }
   
